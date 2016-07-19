5c7122654409d732c2cae38d7f90a3a3ca648810
fabriziofenix <fabrizio.castelli@fao.org>
2016-07-19 14:53:12 +0200

    ready to integrations

--- amis-supply-demand/src/main/java/org/fao/ess/amis/supplydemand/input/SupplyDemandInput.java
+++ amis-supply-demand/src/main/java/org/fao/ess/amis/supplydemand/input/SupplyDemandInput.java
@@ -1,19 +1,64 @@
 package org.fao.ess.amis.supplydemand.input;
 
+import org.fao.ess.amis.supplydemand.utils.data.ConnectionManager;
 import org.fao.fenix.commons.msd.dto.data.Resource;
 import org.fao.fenix.export.core.dto.data.CoreData;
 import org.fao.fenix.export.core.input.plugin.Input;
 
+import java.sql.*;
 import java.util.Map;
 
 public class SupplyDemandInput extends Input {
     @Override
     public void init(Map<String, Object> map, Resource resource) {
 
+        ConnectionManager connectionManager = new ConnectionManager();
+
+
+        connectionManager.init();
+
+
+
+        System.out.println("Init input");
+
+        for(String s: map.keySet()){
+            if(map.get(s)!= null)
+                System.out.println("value: "+map.get(s));
+        }
+
+
+        String query = "select count(*) as c from amis_all_datasources";
+
+        Connection connection = connectionManager.getConnection();
+
+        Statement statement= null;
+        ResultSet rs = null;
+
+
+        try {
+            statement = connection.createStatement();
+
+             rs = statement.executeQuery(query);
+
+            while (rs.next()) {
+                       System.out.println(rs.getString("c"));
+                     }
+
+            rs.close();
+            statement.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public CoreData getResource() {
+
+        System.out.println("here");
+
         return null;
     }
 }
--- amis-supply-demand/src/main/java/org/fao/ess/amis/supplydemand/utils/data/ConnectionManager.java
+++ amis-supply-demand/src/main/java/org/fao/ess/amis/supplydemand/utils/data/ConnectionManager.java
@@ -2,36 +2,61 @@ package org.fao.ess.amis.supplydemand.utils.data;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 
 public class ConnectionManager {
-    private String url,usr,psw;
-
-    private static ThreadLocal<Connection> threadConnection = new ThreadLocal<>();
-    public Connection getConnection() {
-        return threadConnection.get();
-    }
+    private String url, usr, psw;
 
 
-    static {
+    public void init() {
+        Properties prop = new Properties();
+        InputStream input = null;
         try {
+
+            input = getClass().getClassLoader().getResourceAsStream("connection/connection.properties");
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            this.url = prop.getProperty("url");
+            this.usr = prop.getProperty("usr");
+            this.psw = prop.getProperty("psw");
+
             Class.forName("org.postgresql.Driver");
+
+            if (input != null)
+                input.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
     }
 
 
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
 
-    public void init(FilterConfig filterConfig) throws ServletException {
-        ServletContext context = filterConfig.getServletContext();
-        url = context.getInitParameter("url");
-        usr = context.getInitParameter("usr");
-        psw = context.getInitParameter("psw");
+            connection = DriverManager.getConnection("jdbc:postgresql://" + url, usr, psw);
+
+        } catch (SQLException e) {
+
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+
+        }
+        return connection;
     }
 
 }
